8c9b381fdc90a3a14a971a3ee972693196e04af4
eltoncox <elton.unix@gmail.com>
2025-07-15 16:00:46 -0300

    métodos para realizar requisições HTTP.

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({providedIn: 'root'})
+export class ServiceNameService {
+  constructor(private httpClient: HttpClient) { }
+
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class PensamentoService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+
+
 }
