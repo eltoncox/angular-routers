34325eab40eed122bf85f4c31fa890082da5dd78
eltoncox <elton.unix@gmail.com>
2025-07-15 14:22:16 -0300

    interface Pensamento.

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -7,24 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent  implements OnInit {
 
-  listaPensamentos = [
-      {
-        conteudo: 'Passo informações para o componente filho',
-        autoria: 'Componente pai',
-        modelo: 'modelo3'
-      },
-      {
-        conteudo: 'Minha propriedade e decorado com @Input',
-        autoria: 'Componente filho',
-        modelo: 'modelo3'
-      },
-      {
-        conteudo: 'Não se tem uma revolução quando se ama o inimigo; não se tem uma revolução quando se está implorando ao sistema de exploração para que ele te integre. Revoluções derrubam sistemas, revoluções destroem sistemas, Eu não chamo de violência quando é em autodefesa, eu chamo de inteligência.',
-        autoria: 'Malcolm X',
-        modelo: 'modelo1'
-      }
-
-  ];
+  listaPensamentos: Pensamento[] = [];
 
   construtor(){}
 
